80ddc688f98f33bd1f0e1d69332550a0d9499404
user <abdukadyrabdilazov@icloud.com>
2021-12-27 11:16:54 +0600

    Initial commit

---
+++ app/src/main/java/space/abdilazov/hw2/MainActivity.java
@@ -0,0 +1,24 @@
+package space.abdilazov.hw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView.findViewById(R.id.image);
+
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(imageView);
+
+    }
+}
